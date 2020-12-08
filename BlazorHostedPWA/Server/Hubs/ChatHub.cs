4e11dddae113b2f266778f331799cfc64716590e
Asanobm <kim_bumin@fourglobe.co.jp>
2020-12-08 13:37:44 +0900

    fix: method name

--- BlazorHostedPWA/Server/Hubs/ChatHub.cs
+++ BlazorHostedPWA/Server/Hubs/ChatHub.cs
@@ -10,7 +10,7 @@ namespace BlazorHostedPWA.Server.Hubs
   {
     public async Task SendMessage(string user, string message)
     {
-      await Clients.All.SendAsync("ReciveMessage", user, message);
+      await Clients.All.SendAsync("ReceiveMessage", user, message);
     }
   }
 }
